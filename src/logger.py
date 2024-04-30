316e42c8e129900bd84d5f9e0f26d4a06a26241a
unknown <sachthemaster@gmail.com>
2024-04-30 17:18:18 +0530

    data ingestion done

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,8 @@
 import logging
 import os
+from datetime import datetime
 
-
-LOG_FILE="log_file.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
@@ -12,8 +12,3 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO)
-
-logging.info("Logging has started")
-
-
-
